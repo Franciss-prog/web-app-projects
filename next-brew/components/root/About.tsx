50abb96ee9e31579868f170d791dae36d3e7d863
Franciss-prog <francisgil.prog@gmail.com>
2024-12-24 05:29:35 +0800

    reset

--- next-brew/components/root/About.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const About = () => {
-  return (
-    <section className="h-screen"></section>
-)
-}
-
-export default About
--- next-brew/components/root/Selling.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Selling = () => {
-  return <section className="h-screen"></section>;
-};
-
-export default Selling;
